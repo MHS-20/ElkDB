1f02416fd4e6a34fbc790ac44c802a1a73015936
Sentinel <muhamad.salman2001@gmail.com>
2025-02-10 10:24:43 +0100

    (feat) btree: added btree splits

--- storage/kvstore/btree.go
+++ storage/kvstore/btree.go
@@ -13,14 +13,7 @@ type BTree struct {
 	del func(uint64)
 }
 
-// add a new key to a leaf node
-func leafInsert(new BNode, old BNode, idx uint16, key []byte, val []byte) {
-	new.setHeader(BTREE_LEAF, old.nkeys()+1) // setup the header
-	nodeAppendRange(new, old, 0, 0, idx)
-	nodeAppendKV(new, idx, 0, key, val)
-	nodeAppendRange(new, old, idx+1, idx, old.nkeys()-idx)
-}
-
+// search key
 func nodeLookupLE(node BNode, key []byte) uint16 {
 	nkeys := node.nkeys()
 	found := uint16(0)
@@ -35,6 +28,22 @@ func nodeLookupLE(node BNode, key []byte) uint16 {
 		}
 	}
 	return found
+}
+
+/*---- BTREE UPDATES -----*/
+
+// Update a Leaf Node (add a new key to a leaf node)
+func leafInsert(newNode BNode, oldNode BNode, idx uint16, key []byte, val []byte) {
+	newNode.setHeader(BTREE_LEAF, oldNode.nkeys()+1) // setup the header
+
+	// copy existing node keys (oldNode[0:idx] -> newNode[0:idx]
+	nodeAppendRange(newNode, oldNode, 0, 0, idx)
+
+	// insert the new key
+	nodeAppendKV(newNode, idx, 0, key, val)
+
+	// copy the rest of the keys (oldNode[idx:end] -> newNode[idx+1:end+1])
+	nodeAppendRange(newNode, oldNode, idx+1, idx, oldNode.nkeys()-idx)
 }
 
 // copy a KV into its position
@@ -64,7 +73,7 @@ func nodeAppendRange(newNode BNode, oldNode BNode, dstNew uint16, srcOld uint16,
 		return
 	}
 
-	// update all pointers
+	// shift all pointers
 	for i := uint16(0); i < n; i++ {
 		newNode.setPointer(dstNew+i, oldNode.getPointer(srcOld+i))
 	}
@@ -72,13 +81,68 @@ func nodeAppendRange(newNode BNode, oldNode BNode, dstNew uint16, srcOld uint16,
 	// update all offsets
 	dstBegin := newNode.getOffset(dstNew)
 	srcBegin := oldNode.getOffset(srcOld)
-	for i := uint16(1); i <= n; i++ { // ! NOTE: the range is [1, n]
-		offset := dstBegin + oldNode.getOffset(srcOld+i) - srcBegin
+	for i := uint16(1); i <= n; i++ { // the range is [1, n]
+		// relative distance in the old node
+		offset := dstBegin + (oldNode.getOffset(srcOld+i) - srcBegin)
 		newNode.setOffset(dstNew+i, offset)
 	}
 
-	// KVs
+	// copy KVs in interval [srcOld, srcOld+n)
 	begin := oldNode.kvLocation(srcOld)
 	end := oldNode.kvLocation(srcOld + n)
 	copy(newNode[newNode.kvLocation(dstNew):], oldNode[begin:end])
 }
+
+// Update an Internal Node (replace a link with multiple links)
+func nodeReplaceNchild(tree *BTree, newNode BNode, oldNode BNode, idx uint16, childs ...BNode) {
+	inc := uint16(len(childs))
+	newNode.setHeader(BTREE_NODE, oldNode.nkeys()+inc-1)
+	nodeAppendRange(newNode, oldNode, 0, 0, idx)
+
+	// create&add nodes for each child
+	for i, node := range childs {
+		nodeAppendKV(newNode, idx+uint16(i), tree.new(node), node.getKey(0), nil)
+		// first key is used as separator
+	}
+
+	// copy the rest of the keys (oldNode[idx:end] -> newNode[idx+inc:end+inc])
+	nodeAppendRange(newNode, oldNode, idx+inc, idx+1, oldNode.nkeys()-(idx+1))
+}
+
+/*---- BTREE SPLITS -----*/
+// ! just in memory split, save to disk
+func nodeSplit2(left BNode, right BNode, old BNode) {
+	mid := old.nkeys() / 2
+
+	left.setHeader(old.btype(), mid)
+	nodeAppendRange(left, old, 0, 0, mid)
+
+	right.setHeader(old.btype(), old.nkeys()-mid)
+	nodeAppendRange(right, old, 0, mid, old.nkeys()-mid)
+}
+
+// split a node if it's too big. the results are 1~3 nodes.
+func nodeSplit3(old BNode) (uint16, [3]BNode) {
+	if old.nbytes() <= BTREE_MAX_NODE_SIZE {
+		old = old[:BTREE_MAX_NODE_SIZE]
+		return 1, [3]BNode{old}
+	}
+
+	left := make(BNode, 2*BTREE_MAX_NODE_SIZE) // might be split later
+	right := make(BNode, BTREE_MAX_NODE_SIZE)
+	nodeSplit2(left, right, old)
+
+	if left.nbytes() <= BTREE_MAX_NODE_SIZE {
+		left = left[:BTREE_MAX_NODE_SIZE]
+		return 2, [3]BNode{left, right}
+	}
+
+	// the left node is still too large
+	leftleft := make(BNode, BTREE_MAX_NODE_SIZE)
+	middle := make(BNode, BTREE_MAX_NODE_SIZE)
+	nodeSplit2(leftleft, middle, left)
+	assert(leftleft.nbytes() <= BTREE_MAX_NODE_SIZE, "leftleft is too large")
+	return 3, [3]BNode{leftleft, middle, right}
+}
+
+/*---- BTREE INSERTION ----*/
