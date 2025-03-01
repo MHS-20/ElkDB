00eb153d149da153699cc9272108626ad080ce18
Sentinel <muhamad.salman2001@gmail.com>
2025-03-01 11:40:43 +0100

    (fix) freelist: fixed offset error

--- storage/btree/freelist.go
+++ storage/btree/freelist.go
@@ -4,9 +4,9 @@ import "encoding/binary"
 
 type FreeList struct {
 	head uint64
-	get  func(uint64) BNode  // dereference a pointer
-	new  func(BNode) uint64  // append a new page
-	use  func(uint64, BNode) // reuse a page
+	get  func(uint64) BNode
+	new  func(BNode) uint64
+	use  func(uint64, BNode)
 }
 
 const BNODE_FREE_LIST = 3
@@ -15,7 +15,7 @@ const SIZE_LENGTH = 2
 const TOTAL_LENGTH = 8
 const NEXT_LENGTH = 8
 const FREE_LIST_HEADER = TYPE_LENGTH + SIZE_LENGTH + TOTAL_LENGTH + NEXT_LENGTH
-const FREE_LIST_CAP = (BTREE_MAX_NODE_SIZE - FREE_LIST_HEADER) / 8
+const FREE_LIST_CAP = (BTREE_MAX_NODE_SIZE - FREE_LIST_HEADER) / POINTER_SIZE
 
 func (fl *FreeList) ListLen() int {
 	if fl.head == 0 {
@@ -30,10 +30,10 @@ func freeListNodeSize(node BNode) int {
 }
 
 func freeListNext(node BNode) uint64 {
-	return binary.LittleEndian.Uint64(node[FREE_LIST_HEADER:])
+	return binary.LittleEndian.Uint64(node[TYPE_LENGTH+SIZE_LENGTH+TOTAL_LENGTH:])
 }
 
-func freelistNodePointer(node BNode, idx int) uint64 {
+func freelistNodeGetPointer(node BNode, idx int) uint64 {
 	offset := FREE_LIST_HEADER + POINTER_SIZE*idx
 	return binary.LittleEndian.Uint64(node[offset:])
 }
@@ -48,7 +48,7 @@ func freeListNodeSetHeader(node BNode, size uint16, next uint64) {
 	binary.LittleEndian.PutUint16(node[0:], BNODE_FREE_LIST)
 	binary.LittleEndian.PutUint16(node[TYPE_LENGTH:], size)
 	binary.LittleEndian.PutUint64(node[TYPE_LENGTH+SIZE_LENGTH:], 0)
-	binary.LittleEndian.PutUint64(node[FREE_LIST_HEADER:], next)
+	binary.LittleEndian.PutUint64(node[TYPE_LENGTH+SIZE_LENGTH+TOTAL_LENGTH:], next)
 }
 
 func freeListNodeSetTotal(node BNode, total uint64) {
@@ -66,7 +66,7 @@ func (fl *FreeList) Get(topn int) uint64 {
 		assert(next != 0, "node is tail")
 		node = fl.get(next)
 	}
-	return freelistNodePointer(node, freeListNodeSize(node)-topn-1)
+	return freelistNodeGetPointer(node, freeListNodeSize(node)-topn-1)
 }
 
 // remove `popn` pointers and add some new pointers
@@ -95,12 +95,12 @@ func (fl *FreeList) Update(popn int, freed []uint64) {
 			// reuse pointers from the free list itself
 			for remain > 0 && len(reuse)*FREE_LIST_CAP < len(freed)+remain {
 				remain--
-				reuse = append(reuse, freelistNodePointer(node, remain))
+				reuse = append(reuse, freelistNodeGetPointer(node, remain))
 			}
 
 			// move the node into the `freed` list
-			for i := range remain {
-				freed = append(freed, freelistNodePointer(node, i))
+			for i := 0; i < remain; i++ {
+				freed = append(freed, freelistNodeGetPointer(node, i))
 			}
 		}
 
@@ -127,11 +127,11 @@ func freeListPush(fl *FreeList, freed []uint64, reuse []uint64) {
 		freed = freed[size:]
 
 		if len(reuse) > 0 {
-			// reuse pointer from the list
+			// reuse pointer
 			fl.head, reuse = reuse[0], reuse[1:]
 			fl.use(fl.head, newNode)
 		} else {
-			// append a page for the new node
+			// append page for new node
 			fl.head = fl.new(newNode)
 		}
 	}
