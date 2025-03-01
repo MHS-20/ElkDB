cf10a5e347e460c8312e1fb6eaea8cb262b1d081
Sentinel <muhamad.salman2001@gmail.com>
2025-03-01 11:39:40 +0100

    (test) pager: added freelist to pager test

--- storage/btree/pager_test.go
+++ storage/btree/pager_test.go
@@ -18,12 +18,12 @@ type PagerTester struct {
 func newPagerTester() *PagerTester {
 	os.Remove("test.db")
 
-	d := &PagerTester{}
-	d.ref = map[string]string{}
-	d.db.Path = "test.db"
-	err := d.db.Open()
+	pt := &PagerTester{}
+	pt.ref = map[string]string{}
+	pt.db.Path = "test.db"
+	err := pt.db.Open()
 	assert(err == nil, "open failed")
-	return d
+	return pt
 }
 
 func (pt *PagerTester) dispose() {
@@ -57,6 +57,7 @@ func (pt *PagerTester) dump() ([]string, []string) {
 				vals = append(vals, string(node.getVal(i)))
 			}
 		} else {
+			assert(node.btype() == BTREE_NODE, "invalid node type")
 			for i := uint16(0); i < nkeys; i++ {
 				ptr := node.getPointer(i)
 				nodeDump(ptr)
@@ -65,15 +66,29 @@ func (pt *PagerTester) dump() ([]string, []string) {
 	}
 
 	nodeDump(pt.db.tree.root)
-	assert(keys[0] == "", "root is not leaf")
-	assert(vals[0] == "", "root is not leaf")
+	assert(keys[0] == "", "invalid root key")
+	assert(vals[0] == "", "invalid root val")
 	return keys[1:], vals[1:]
+}
+
+func flDump(fl *FreeList) []uint64 {
+	ptrs := []uint64{}
+	head := fl.head
+	for head != 0 {
+		node := fl.get(head)
+		size := freeListNodeSize(node)
+		for i := 0; i < size; i++ {
+			ptrs = append(ptrs, freelistNodeGetPointer(node, i))
+		}
+		head = freeListNext(node)
+	}
+	return ptrs
 }
 
 func (pt *PagerTester) verify(t *testing.T) {
 	keys, vals := pt.dump()
-
 	rkeys, rvals := []string{}, []string{}
+
 	for k, v := range pt.ref {
 		rkeys = append(rkeys, k)
 		rvals = append(rvals, v)
@@ -93,27 +108,48 @@ func (pt *PagerTester) verify(t *testing.T) {
 	is.Equal(t, rkeys, keys)
 	is.Equal(t, rvals, vals)
 
+	// node structures
+	pages := make([]uint8, pt.db.page.flushed)
+	pages[0] = 1
+	pages[pt.db.tree.root] = 1
 	var nodeVerify func(BNode)
+
 	nodeVerify = func(node BNode) {
 		nkeys := node.nkeys()
-		assert(nkeys >= 1, "nkeys < 1")
-
+		assert(nkeys >= 1, "invalid number of keys")
 		if node.btype() == BTREE_LEAF {
 			return
 		}
-
 		for i := uint16(0); i < nkeys; i++ {
+			ptr := node.getPointer(i)
+			is.Zero(t, pages[ptr])
+			pages[ptr] = 1
 			key := node.getKey(i)
-			kid := pt.db.tree.get(node.getPointer(i))
+			kid := pt.db.tree.get(ptr)
 			is.Equal(t, key, kid.getKey(0))
 			nodeVerify(kid)
 		}
 	}
+
 	nodeVerify(pt.db.tree.get(pt.db.tree.root))
+
+	// free list
+	for head := pt.db.free.head; head != 0; {
+		is.Zero(t, pages[head])
+		pages[head] = 2
+		head = freeListNext(pt.db.pageGet(head))
+	}
+	for _, ptr := range flDump(&pt.db.free) {
+		is.Zero(t, pages[ptr])
+		pages[ptr] = 3
+	}
+	for _, flag := range pages {
+		is.NotZero(t, flag)
+	}
 }
 
-func TestKVBasic(t *testing.T) {
-	fmt.Println("TestKVBasic")
+func TestPagerBasic(t *testing.T) {
+	fmt.Println("TestPagerBasic")
 	pt := newPagerTester()
 	defer pt.dispose()
 
@@ -125,9 +161,7 @@ func TestKVBasic(t *testing.T) {
 		key := fmt.Sprintf("key%d", fmix32(uint32(i)))
 		val := fmt.Sprintf("vvv%d", fmix32(uint32(-i)))
 		pt.add(key, val)
-		pt.verify(t)
-
-		if i < 20 {
+		if i < 2000 {
 			pt.verify(t)
 		}
 	}
@@ -135,7 +169,7 @@ func TestKVBasic(t *testing.T) {
 	pt.verify(t)
 	t.Log("insertion done")
 
-	// deletion
+	// del
 	for i := 20; i < 25; i++ {
 		key := fmt.Sprintf("key%d", fmix32(uint32(i)))
 		is.True(t, pt.del(key))
@@ -166,8 +200,8 @@ func TestKVBasic(t *testing.T) {
 	pt.verify(t)
 }
 
-func TestKVRandLength(t *testing.T) {
-	fmt.Println("TestKVRandLength")
+func TestPagerRandLength(t *testing.T) {
+	fmt.Println("TestPagerRandLength")
 	pt := newPagerTester()
 	defer pt.dispose()
 
@@ -187,8 +221,8 @@ func TestKVRandLength(t *testing.T) {
 	}
 }
 
-func TestKVIncLength(t *testing.T) {
-	fmt.Println("TestKVIncLength")
+func TestPagerIncLength(t *testing.T) {
+	fmt.Println("TestPagerIncLength")
 	for l := 1; l < BTREE_MAX_KEY_SIZE+BTREE_MAX_VAL_SIZE; l++ {
 		pt := newPagerTester()
 
@@ -196,19 +230,17 @@ func TestKVIncLength(t *testing.T) {
 		if klen > BTREE_MAX_KEY_SIZE {
 			klen = BTREE_MAX_KEY_SIZE
 		}
-
 		vlen := l - klen
 		key := make([]byte, klen)
 		val := make([]byte, vlen)
 
 		factor := BTREE_MAX_NODE_SIZE / l
-		size := min(factor*factor*2, 40)
-		size = max(size, 10)
-
-		for i := 0; i < size; i++ {
+		size := max(min(factor*factor*2, 40), 10)
+		for range size {
 			rand.Read(key)
 			pt.add(string(key), string(val))
 		}
+
 		pt.verify(t)
 		pt.dispose()
 	}
