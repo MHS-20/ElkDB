e8bd8b1270c8e882c8cf38e6dd80de93af0a60c9
MHS <muhamad.salman2001@gmail.com>
2025-09-05 21:14:50 +0200

    test: added btree iter test

---
+++ storage/btree_iter_test.go
@@ -0,0 +1,68 @@
+package storage
+
+import (
+	"fmt"
+	"testing"
+
+	is "github.com/stretchr/testify/require"
+)
+
+func TestBTreeIter(t *testing.T) {
+	{
+		tt := newTreeTester()
+		iter := tt.tree.SeekLE(nil)
+		is.False(t, iter.Valid())
+	}
+
+	fmt.Println("BIter test")
+
+	sizes := []int{5, 2500}
+	for _, sz := range sizes {
+		tt := newTreeTester()
+
+		for i := 0; i < sz; i++ {
+			key := fmt.Sprintf("key%010d", i)
+			val := fmt.Sprintf("vvv%d", fmix32(uint32(-i)))
+			tt.add(key, val)
+		}
+		tt.verify(t)
+
+		prevk, prevv := []byte(nil), []byte(nil)
+		for i := 0; i < sz; i++ {
+			key := []byte(fmt.Sprintf("key%010d", i))
+			val := []byte(fmt.Sprintf("vvv%d", fmix32(uint32(-i))))
+			// fmt.Println(i, string(key), val)
+
+			iter := tt.tree.SeekLE(key)
+			is.True(t, iter.Valid())
+			gotk, gotv := iter.Deref()
+			is.Equal(t, key, gotk)
+			is.Equal(t, val, gotv)
+
+			iter.Prev()
+			if i > 0 {
+				is.True(t, iter.Valid())
+				gotk, gotv := iter.Deref()
+				is.Equal(t, prevk, gotk)
+				is.Equal(t, prevv, gotv)
+			} else {
+				is.False(t, iter.Valid())
+			}
+
+			iter.Next()
+			{
+				is.True(t, iter.Valid())
+				gotk, gotv := iter.Deref()
+				is.Equal(t, key, gotk)
+				is.Equal(t, val, gotv)
+			}
+
+			if i+1 == sz {
+				iter.Next()
+				is.False(t, iter.Valid())
+			}
+
+			prevk, prevv = key, val
+		}
+	}
+}
