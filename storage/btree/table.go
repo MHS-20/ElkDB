954deb87750bfbd8d8429d5afe59d4cf76e22161
MHS <muhamad.salman2001@gmail.com>
2025-09-05 15:51:41 +0200

    feat: added internal tables definition

--- storage/btree/table.go
+++ storage/btree/table.go
@@ -1,4 +1,4 @@
-package btree 
+package btree
 
 import (
 	"bytes"
@@ -20,4 +20,38 @@ type TableDef struct {
 	PKeys  int      // the first PKeys columns are the primary key
 	Prefix uint32   // key prefixes for different tables in the same B-tree
 }
- 
+
+// Table cells supports only int64 and strings
+const (
+	TYPE_ERROR = 0
+	TYPE_BYTES = 1
+	TYPE_INT64 = 2
+)
+
+/*--------- INTERNAL TABLES ---------*/
+// metadata internal table
+// used to store the next available table prefix
+var TDEF_META = &TableDef{
+	Prefix: 1,
+	Name:   "@meta",
+	Types:  []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:   []string{"key", "val"},
+	PKeys:  1,
+}
+
+// table schemas internal table
+// serialized table definitions
+var TDEF_TABLE = &TableDef{
+	Prefix: 2,
+	Name:   "@table",
+	Types:  []uint32{TYPE_BYTES, TYPE_BYTES},
+	Cols:   []string{"name", "def"},
+	PKeys:  1,
+}
+
+var INTERNAL_TABLES map[string]*TableDef = map[string]*TableDef{
+	"@meta":  TDEF_META,
+	"@table": TDEF_TABLE,
+}
+
+const TABLE_PREFIX_MIN = 100
