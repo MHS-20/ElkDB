fbdc1af0b3e4d5bc81fc6e85711d80028dea7047
MHS <muhamad.salman2001@gmail.com>
2025-09-05 21:03:58 +0200

    feat: added iter movement to next

--- storage/btree_iter.go
+++ storage/btree_iter.go
@@ -41,6 +41,15 @@ func (iter *BIter) Valid() bool {
 	return iter.pos[last] < node.nkeys()
 }
 
+func (iter *BIter) Prev() {
+	iterPrev(iter, len(iter.path)-1)
+}
+
+func (iter *BIter) Next() {
+	iterNext(iter, len(iter.path)-1)
+}
+
+
 func iterPrev(iter *BIter, level int) {
 	if iter.pos[level] > 0 {
 		iter.pos[level]-- // move within this node
@@ -56,5 +65,23 @@ func iterPrev(iter *BIter, level int) {
 		kid := iter.tree.get(node.getPointer(iter.pos[level]))
 		iter.path[level+1] = kid
 		iter.pos[level+1] = kid.nkeys() - 1
+	}
+}
+
+func iterNext(iter *BIter, level int) {
+	if iter.pos[level]+1 < iter.path[level].nkeys() {
+		iter.pos[level]++
+	} else if level > 0 {
+		iterNext(iter, level-1)
+	} else {
+		iter.pos[len(iter.pos)-1]++ // past the last key
+		return
+	}
+
+	if level+1 < len(iter.pos) {
+		node := iter.path[level]
+		kid := iter.tree.get(node.getPtr(iter.pos[level]))
+		iter.path[level+1] = kid
+		iter.pos[level+1] = 0
 	}
 }
