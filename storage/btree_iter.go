3a40011a540335f04f5b4f3abccf1c179c17f4d9
MHS <muhamad.salman2001@gmail.com>
2025-09-05 20:54:53 +0200

    feat: added iter dereferencing and validation

--- storage/btree_iter.go
+++ storage/btree_iter.go
@@ -14,3 +14,29 @@ func (iter *BIter) Clone() *BIter {
 		pos:  append([]uint16(nil), iter.pos...),
 	}
 }
+
+// return current KV pair
+func (iter *BIter) Deref() ([]byte, []byte) {
+	assert(iter.Valid(), "invalid iterator")
+	last := len(iter.path) - 1
+	node := iter.path[last]
+	pos := iter.pos[last]
+	return node.getKey(pos), node.getVal(pos)
+}
+
+func (iter *BIter) Valid() bool {
+	// the first key in the tree is not real (dummy)
+	dummy := true
+	for _, pos := range iter.pos {
+		if pos != 0 {
+			dummy = false
+		}
+	}
+	if dummy {
+		return false
+	}
+
+	last := len(iter.path) - 1
+	node := iter.path[last]
+	return iter.pos[last] < node.nkeys()
+}
