d6d7d1e4fa617d6183354dcfc36ee5749f0a8ffd
Muhamad Salman <muhamad.salman2001@gmail.com>
2026-05-30 15:24:06 +0200

    feat: added row encoding decoding

--- src/table.go
+++ src/table.go
@@ -1,6 +1,9 @@
 package elk
 
 import (
+	"bytes"
+	"encoding/binary"
+	"fmt"
 	"sync"
 )
 
@@ -75,4 +78,183 @@ type Value struct {
 type Record struct {
 	Cols []string
 	Vals []Value
+}
+
+func (rec *Record) AddStr(key string, val []byte) *Record {
+	rec.Cols = append(rec.Cols, key)
+	rec.Vals = append(rec.Vals, Value{Type: TYPE_BYTES, Str: val})
+	return rec
+}
+
+func (rec *Record) AddInt64(key string, val int64) *Record {
+	rec.Cols = append(rec.Cols, key)
+	rec.Vals = append(rec.Vals, Value{Type: TYPE_INT64, I64: val})
+	return rec
+}
+
+// rearrange a record to the defined column order
+func reorderRecord(tdef *TableDef, rec Record) ([]Value, error) {
+	assert(len(rec.Cols) == len(rec.Vals))
+	out := make([]Value, len(tdef.Cols))
+	for i, c := range tdef.Cols {
+		v := rec.Get(c)
+		if v == nil {
+			continue // leave this column uninitialized
+		}
+		if v.Type != tdef.Types[i] {
+			return nil, fmt.Errorf("bad column type: %s", c)
+		}
+		out[i] = *v
+	}
+	return out, nil
+}
+
+func valuesComplete(tdef *TableDef, vals []Value, n int) error {
+	for i, v := range vals {
+		if i < n && v.Type == 0 {
+			return fmt.Errorf("missing column: %s", tdef.Cols[i])
+		} else if i >= n && v.Type != 0 {
+			return fmt.Errorf("extra column: %s", tdef.Cols[i])
+		}
+	}
+	return nil
+}
+
+// reorder a record and check for missing columns.
+// n == tdef.PKeys: record is exactly a primary key
+// n == len(tdef.Cols): record contains all columns
+func checkRecord(tdef *TableDef, rec Record, n int) ([]Value, error) {
+	vals, err := reorderRecord(tdef, rec)
+	if err != nil {
+		return nil, err
+	}
+	err = valuesComplete(tdef, vals, n)
+	if err != nil {
+		return nil, err
+	}
+	return vals, nil
+}
+
+// check column types
+func checkRecordTypes(tdef *TableDef, rec Record) error {
+	for i, c := range rec.Cols {
+		j := colIndex(tdef, c)
+		if j < 0 || tdef.Types[j] != rec.Vals[i].Type {
+			return fmt.Errorf("bad column: %s", c)
+		}
+	}
+	return nil
+}
+
+//  1. strings are encoded as null-terminated strings,
+//     escape the null byte so that strings contain no null byte.
+//  2. "\xff" represents the highest order in key comparisons,
+//     also escape the first byte if it's 0xff.
+func escapeString(in []byte) []byte {
+	first := len(in) > 0 && in[0] >= 0xfe
+	zeros := bytes.Count(in, []byte{0})
+	ones := bytes.Count(in, []byte{1})
+	if !first && zeros+ones == 0 {
+		return in
+	}
+
+	nescape := zeros + ones
+	if first {
+		nescape++
+	}
+	out := make([]byte, len(in)+nescape)
+
+	pos := 0
+	if first {
+		out[0] = 0xfe
+		out[1] = in[0]
+		pos += 2
+		in = in[1:]
+	}
+
+	for _, ch := range in {
+		if ch <= 1 {
+			out[pos+0] = 0x01
+			out[pos+1] = ch + 1
+			pos += 2
+		} else {
+			out[pos] = ch
+			pos += 1
+		}
+	}
+	return out
+}
+
+func unescapeString(in []byte) []byte {
+	assert(len(in) == 0 || in[0] != 0xff)
+	first := len(in) > 0 && in[0] == 0xfe
+	if !first && bytes.Count(in, []byte{1}) == 0 {
+		return in
+	}
+
+	out := make([]byte, len(in))
+	pos := 0
+	if first {
+		out[0] = in[1]
+		pos++
+		in = in[2:]
+	}
+	for i := 0; i < len(in); i++ {
+		if in[i] == 0x01 {
+			i++
+			assert(in[i] >= 1)
+			out[pos] = in[i] - 1
+		} else {
+			out[pos] = in[i]
+		}
+		pos++
+	}
+	return out[:pos]
+}
+
+// order-preserving encoding
+func encodeValues(out []byte, vals []Value) []byte {
+	for _, v := range vals {
+		switch v.Type {
+		case TYPE_INT64:
+			var buf [8]byte
+			u := uint64(v.I64) + (1 << 63)
+			binary.BigEndian.PutUint64(buf[:], u)
+			out = append(out, buf[:]...)
+		case TYPE_BYTES:
+			out = append(out, escapeString(v.Str)...)
+			out = append(out, 0) // null-terminated
+		default:
+			panic("what?")
+		}
+	}
+	return out
+}
+
+// for primary keys or index keys
+func encodeKey(out []byte, prefix uint32, vals []Value) []byte {
+	var buf [4]byte
+	binary.BigEndian.PutUint32(buf[:], prefix)
+	out = append(out, buf[:]...)
+	out = encodeValues(out, vals)
+	return out
+}
+
+func decodeValues(in []byte, out []Value) {
+	for i := range out {
+		switch out[i].Type {
+		case TYPE_INT64:
+			u := binary.BigEndian.Uint64(in[:8])
+			out[i].I64 = int64(u - (1 << 63))
+			in = in[8:]
+		case TYPE_BYTES:
+			idx := bytes.IndexByte(in, 0)
+			assert(idx >= 0)
+			out[i].Str = unescapeString(in[:idx:idx])
+			in = in[idx+1:]
+		default:
+			panic("what?")
+		}
+	}
+	assert(len(in) == 0)
 }
